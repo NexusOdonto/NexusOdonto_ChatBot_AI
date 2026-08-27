d0cd99facc5ea5996718fcbb989c2e56b9b7516f
kvinnxz <keviin9856@gmail.com>
2026-08-27 09:27:11 -0500

    feat: :sparkles: add security check node to prevent prompt injections and jailbreaks

--- app/graph/state.py
+++ app/graph/state.py
@@ -1,4 +1,4 @@
-from typing import Annotated, NotRequired, TypedDict
+from typing import Annotated, NotRequired, TypedDict, Optional
 
 from langchain_core.messages import AnyMessage
 from langgraph.graph.message import add_messages
@@ -9,5 +9,5 @@ class AgentState(TypedDict):
 
 	# add_messages conserva el historial y agrega los mensajes nuevos.
 	messages: Annotated[list[AnyMessage], add_messages] # Lista de mensajes que conforman el historial de la conversación. Cada mensaje puede ser del tipo HumanMessage, AIMessage, SystemMessage, o ToolMessage.
-	conversation_status: NotRequired[str]
+	conversation_status: NotRequired[Optional[str]]
 	rag_confidence: NotRequired[float]
